a0f6787e063561cdafca4459790238d0a109ea7e
Valvalv <62302920+Valeria-V@users.noreply.github.com>
2023-05-08 15:57:34 -0300

    upload project

---
+++ tcc/js/perfil.js
@@ -0,0 +1,46 @@
+  // decodificar token salvo
+  const token = localStorage.getItem("token");
+  console.log(token);
+  
+  if (token) {
+    try {
+      const decodedToken = jwt.jwtDecode(token);
+      console.log(decodedToken);
+  
+      // pegar informações com o token salvo
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      };
+      
+      // trazer resposta da api por array
+      const perfil = localStorage.getItem("perfil");
+      // trazer resposta da api por array
+      fetch(perfil, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // montar lista de usuários
+        const usersContainer = document.getElementById("perfil");
+        let usersHTML = '';
+          usersHTML += `
+            <div class="user master" data-user-id="${data.Id}">
+              <div class="avatar-perfil" onclick="abrirImagem()"></div>
+              <h5>${data.FirstName} ${data.LastName}</h5>
+              <p>Endereço: <span>${data.Address}</span><p>
+              <p>E-mail: <span>${data.email}</span><p>
+              <p>Telefone: <span>${data.Phone}</span><p>
+              <p>Data de criação: <span>${data.SignUpDate}</span><p>
+            </div>
+          `;
+        usersContainer.innerHTML = usersHTML;
+      });
+    } catch (err) {
+        console.log("Erro ao decodificar o token", err);
+      }
+  } else {
+    console.log("Token não encontrado na storage");
+  }
